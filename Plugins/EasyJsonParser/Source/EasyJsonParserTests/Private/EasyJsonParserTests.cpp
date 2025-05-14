a7326b59cc6058e4cf87b6c5dc92928e51cf5543
ayumax <develop@ayumax.net>
2025-05-14 23:26:49 +0900

    Add EasyJsonParserTests module and setup testing framework
    Introduced a new module, EasyJsonParserTests, to enable testing functionality for the EasyJsonParser plugin. The module includes necessary dependencies, basic startup/shutdown structure, and is listed as a DeveloperTool in the plugin configuration. Also, updated file structure to separate test-related code.

---
+++ Plugins/EasyJsonParser/Source/EasyJsonParserTests/Private/EasyJsonParserTests.cpp
@@ -0,0 +1,12 @@
+// Copyright 2019 ayumax. All Rights Reserved.
+#include "EasyJsonParserTests.h"
+
+IMPLEMENT_MODULE(FEasyJsonParserTestsModule, EasyJsonParserTests)
+
+void FEasyJsonParserTestsModule::StartupModule()
+{
+}
+
+void FEasyJsonParserTestsModule::ShutdownModule()
+{
+}
