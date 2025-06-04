20e0a3d84024355bb0375a03ed3d43dbe599d4f2
ayumax <develop@ayumax.net>
2025-06-04 10:56:10 +0900

    updated to support ue5.6

--- Froola/run_create_packages.bat
+++ Froola/run_create_packages.bat
@@ -1,4 +1,4 @@
 cd /d %~dp0
+Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v UE_5_6 -u git@github.com:ayumax/EasyJsonParser.git -b UE5.6 -t -c -o packages
 Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v UE_5_5 -u git@github.com:ayumax/EasyJsonParser.git -b UE5.5 -t -c -o packages
 Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v UE_5_4 -u git@github.com:ayumax/EasyJsonParser.git -b UE5.4 -t -c -o packages
-Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v UE_5_3 -u git@github.com:ayumax/EasyJsonParser.git -b UE5.3 -t -c -o packages
