66e304b3eae4939492849c05f182cab725738bbf
ayumax <develop@ayumax.net>
2025-06-22 21:14:04 +0900

    Updated

--- Froola/run_create_packages.bat
+++ Froola/run_create_packages.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v 5.4,5.5,5.6 -e Windows,Mac,Linux -u git@github.com:ayumax/EasyJsonParser.git -t -c -o packages --copy-package-after-build
+Froola.exe plugin -n EasyJsonParser -p EasyJsonParserTest -v 5.4,5.5,5.6 -e Windows,Mac,Linux -u git@github.com:ayumax/EasyJsonParser.git -t -c -o pa
